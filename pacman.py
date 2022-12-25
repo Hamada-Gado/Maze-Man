004742c49c6ca9940d679d19cd5f70b7d49c3627
Hamada-Gado <108872265+Hamada-Gado@users.noreply.github.com>
2022-12-25 07:28:22 -0800

    Add specific ghosts & maze

--- pacman.py
+++ pacman.py
@@ -1,5 +1,6 @@
-import random
-import time
+from abc import ABC, abstractmethod
+import os
+import random, time, sys
 import tkinter as tk
 from typing import Callable
 
@@ -24,7 +25,10 @@ class Game:
         self.pellet: Pellet = Pellet(self)
 
         self.ghosts: list[Ghost] = []
-        self.ghosts.append(Ghost(self, "red"))
+        self.ghosts.append(Orange_Ghost(self))
+        self.ghosts.append(Red_Ghost(self))
+        self.ghosts.append(Blue_Ghost(self))
+        self.ghosts.append(Pink_Ghost(self))
 
         self.delta_time: float = 0
          
@@ -35,7 +39,10 @@ class Game:
         self.pellet = Pellet(self)
 
         self.ghosts = []
-        self.ghosts.append(Ghost(self, "red"))
+        self.ghosts.append(Orange_Ghost(self))
+        self.ghosts.append(Red_Ghost(self))
+        self.ghosts.append(Blue_Ghost(self))
+        self.ghosts.append(Pink_Ghost(self))
 
         self.root.after(0, self._run)
         self.delta_time = time.time()
@@ -59,6 +66,7 @@ class Game:
         self.maze.update()
         self.pellet.update()
         self.player.update(self.delta_time)
+
         for ghost in self.ghosts:
             ghost.update(self.delta_time)
 
@@ -79,7 +87,6 @@ class Game:
 
         for ghost in self.ghosts:
             if aabb_collision(ghost.coordinate.position, position):
-                print("lol again")
                 return True
 
         return False
@@ -132,10 +139,11 @@ class Coordinate:
 class Maze:
     BLOCK_WIDTH: int = 15 
     BLOCK_HEIGHT: int = 15
-    COLS: int = Game.WIDTH//BLOCK_WIDTH # 20
-    ROWS: int = Game.HEIGHT//BLOCK_HEIGHT # 20
+    COLS: int = Game.WIDTH//BLOCK_WIDTH # 40
+    ROWS: int = Game.HEIGHT//BLOCK_HEIGHT # 40
     BG_COLOR: str = "black"
     BLOCK_COLOR: str = "blue"
+    MAZE_FILE_PATH: str = sys.path[0] + "\\maze.txt"
 
     def __init__(self, master: Game) -> None:
         self.master: Game = master
@@ -143,18 +151,16 @@ class Maze:
         self.make_maze()
 
     def make_maze(self) -> None:
-        for x in range(0, -2, -1):
-            for y in range(Maze.ROWS):
-                self.maze[x][y] = 1
-        
-        for x in range(Maze.COLS):
-            for y in range(0, -2, -1):
-                self.maze[x][y] = 1
 
-        self.maze[5][-1] = 0
-        self.maze[6][-1] = 0
-        self.maze[5][0] = 0
-        self.maze[6][0] = 0
+        with open(Maze.MAZE_FILE_PATH, "r") as f:
+            data = f.read()
+
+        data = data.split("\n")
+
+        for x in range(Maze.COLS):
+            line = data[x].split(" ")
+            for y in range(Maze.ROWS):
+                self.maze[x][y] = int(line[y])
     
     def update(self) -> None:
         # draw
@@ -195,6 +201,12 @@ class PacMan:
 
     def update(self, dt: float) -> None:
         # move
+        self.move(dt)
+
+        # draw
+        self.draw()
+
+    def move(self, dt: float) -> None:
         if self.master.keys.get("Right", False):
             predicted_position = self.coordinate.predict_move(PacMan.SPEED * dt, 0)
             if not self.master.maze.check_collision(predicted_position):
@@ -219,10 +231,7 @@ class PacMan:
                 self.coordinate.move(0, -PacMan.SPEED * dt)
             self.angle = 90
 
-        self.frames_timer += dt
-
-        # draw
-        self.draw()
+        self.frames_timer += dt 
 
     def draw(self) -> None:
         if self.frames_timer > 0.1:
@@ -260,25 +269,118 @@ class Pellet:
     def draw(self) -> None:
         self.master.canvas.create_oval(self.coordinate.position, fill= Pellet.COLOR, outline= Pellet.COLOR)
 
-class Ghost:
+class Ghost(ABC):
     WIDTH: int = 20
     HEIGHT: int = 20
-    SPEED: int = 150
+    SPEED: int = 120
+    SCARED_COLOR: str = "#0000FF"
     EYE_OUT_COLOR: str = "white"
     EYE_IN_COLOR: str = "black"
     DIRECTION: list[str] = ["right", "left", "up", "down"]
 
-    def __init__(self, master: Game, color) -> None:
+    def __init__(self, master: Game) -> None:
         self.master: Game = master
         self.coordinate: Coordinate = Coordinate(0, 0, Ghost.WIDTH, Ghost.HEIGHT)
         while self.master.maze.check_collision(self.coordinate.position):
             self.coordinate.set_random_position()
-        self.color: str = color
+        self.color: str = Ghost.SCARED_COLOR
         self.direction: str = "right"
 
     def update(self, dt: float) -> None:
         # move
+        self.move(dt)
+
+        # draw
+        self.draw()
+
+    @abstractmethod
+    def move(self, dt: float) -> None:
+        pass
+
+    def draw(self) -> None:
+        self._draw_head()
+        self._draw_body()
+        self._draw_eyes()
+
+    def _draw_head(self) -> None:
+        self.master.canvas.create_arc(self.coordinate.position, start= 0, extent= 180, fill= self.color, outline= self.color)
+
+
+    def _draw_body(self) -> None:
+        x0, y0, x1, y1 = self.coordinate.position
+        self.master.canvas.create_rectangle(x0, y0 + 10, x1, y1, fill= self.color, outline= self.color)
+
+    def _draw_eyes(self) -> None:
+        x0, y0, x1, y1 = self.coordinate.position
+        x0 += 3
+        y0 += 3
+        x1 = x0 + 5
+        y1 = y0 + 5
+        self.master.canvas.create_oval(x0, y0, x1, y1, fill= Ghost.EYE_OUT_COLOR, outline= Ghost.EYE_OUT_COLOR)
+        self.master.canvas.create_oval(x0 + 9, y0, x1 + 9, y1, fill= Ghost.EYE_OUT_COLOR, outline= Ghost.EYE_OUT_COLOR)
+
+        if self.direction == "right":
+            dx = 1
+            dy = 0
+        elif self.direction == "left":
+            dx = -1
+            dy = 0
+        elif self.direction == "down":
+            dx = 0
+            dy = 1
+        else:
+           dx = 0
+           dy = -1
+
+        self.master.canvas.create_oval(x0 + dx, y0 + dy, x1 + dx, y1 + dy, fill= Ghost.EYE_IN_COLOR, outline= Ghost.EYE_IN_COLOR)
+        self.master.canvas.create_oval(x0 + 9 +dx, y0 + dy, x1 + 9 + dx, y1 + dy, fill= Ghost.EYE_IN_COLOR, outline= Ghost.EYE_IN_COLOR)
+
+class Red_Ghost(Ghost):
+    COLOR: str = "red"
+
+    def __init__(self, master: Game) -> None:
+        super().__init__(master)
+        self.color = Red_Ghost.COLOR
+
+    # Override
+    def move(self, dt: float) -> None:
+        distanceX: float = self.master.player.coordinate.position[0] - self.coordinate.position[0]
+        distanceY: float = self.master.player.coordinate.position[1] - self.coordinate.position[1]
+
+        if distanceX > 10:
+            dx = 1
+        elif distanceX < -10:
+            dx = -1
+        else:
+            dx = 0
+
+        if distanceY > 10:
+            dy = 1
+        elif distanceY < -10:
+            dy = -1
+        else:
+            dy = 0
+        
+        predict_position = self.coordinate.predict_move(dx * Ghost.SPEED * dt, 0)
+        if not self.master.maze.check_collision(predict_position):
+            self.coordinate.move(dx * Ghost.SPEED * dt, 0)
+
+        predict_position = self.coordinate.predict_move(0, dy * Ghost.SPEED * dt)
+        if not self.master.maze.check_collision(predict_position):
+            self.coordinate.move(0, dy * Ghost.SPEED * dt)
+
+
+class Orange_Ghost(Ghost):
+    COLOR: str = "orange"
+
+    def __init__(self, master: Game) -> None:
+        super().__init__(master)
+        self.color = Orange_Ghost.COLOR
+
+    # Override
+    def move(self, dt: float) -> None:
         random.shuffle(Ghost.DIRECTION)
+        
         if self.direction == "right":
             predict_position = self.coordinate.predict_move(Ghost.SPEED * dt, 0)
             if self.master.maze.check_collision(predict_position):
@@ -305,48 +407,49 @@ class Ghost:
             if self.master.maze.check_collision(predict_position):
                 self.direction = Ghost.DIRECTION[0]
             else:
-                self.coordinate.move(0, -Ghost.SPEED*dt)   
+                self.coordinate.move(0, -Ghost.SPEED*dt)
 
-        # draw
-        self.draw()
+class Blue_Ghost(Ghost):
+    COLOR: str = "cyan"
 
-    def draw(self) -> None:
-        self._draw_head()
-        self._draw_body()
-        self._draw_eyes()
+    def __init__(self, master: Game) -> None:
+        super().__init__(master)
+        self.color = Blue_Ghost.COLOR
 
-    def _draw_head(self) -> None:
-        self.master.canvas.create_arc(self.coordinate.position, start= 0, extent= 180, fill= self.color, outline= self.color)
+    # Override
+    def move(self, dt: float) -> None:
+        distanceX: float = self.master.player.coordinate.position[0] - self.coordinate.position[0]
+        distanceY: float = self.master.player.coordinate.position[1] - self.coordinate.position[1]
 
-
-    def _draw_body(self) -> None:
-        x0, y0, x1, y1 = self.coordinate.position
-        self.master.canvas.create_rectangle(x0, y0 + 10, x1, y1, fill= self.color, outline= self.color)
-
-    def _draw_eyes(self) -> None:
-        x0, y0, x1, y1 = self.coordinate.position
-        x0 += 3
-        y0 += 3
-        x1 = x0 + 5
-        y1 = y0 + 5
-        self.master.canvas.create_oval(x0, y0, x1, y1, fill= Ghost.EYE_OUT_COLOR, outline= Ghost.EYE_OUT_COLOR)
-        self.master.canvas.create_oval(x0 + 8, y0, x1 + 8, y1, fill= Ghost.EYE_OUT_COLOR, outline= Ghost.EYE_OUT_COLOR)
-
-        if self.direction == "right":
+        if distanceX > 10:
             dx = 1
-            dy = 0
-        elif self.direction == "left":
+        elif distanceX < -10:
             dx = -1
-            dy = 0
-        elif self.direction == "down":
-            dx = 0
-            dy = 1
         else:
-           dx = 0
-           dy = -1
+            dx = 0
 
-        self.master.canvas.create_oval(x0 + dx, y0 + dy, x1 + dx, y1 + dy, fill= Ghost.EYE_IN_COLOR, outline= Ghost.EYE_IN_COLOR)
-        self.master.canvas.create_oval(x0 + 8 +dx, y0 + dy, x1 + 8 + dx, y1 + dy, fill= Ghost.EYE_IN_COLOR, outline= Ghost.EYE_IN_COLOR)
+        if distanceY > 10:
+            dy = 1
+        elif distanceY < -10:
+            dy = -1
+        else:
+            dy = 0
+        
+        predict_position = self.coordinate.predict_move(dx * Ghost.SPEED * dt, 0)
+        if not self.master.maze.check_collision(predict_position) and dx != 0:
+            self.coordinate.move(dx * Ghost.SPEED * dt, 0)
+        else:
+            predict_position = self.coordinate.predict_move(0, dy * Ghost.SPEED * dt)
+            if not self.master.maze.check_collision(predict_position):
+                self.coordinate.move(0, dy * Ghost.SPEED * dt)
+
+class Pink_Ghost(Blue_Ghost):
+    COLOR: str = "pink"
+
+    def __init__(self, master: Game) -> None:
+        super().__init__(master)
+        self.color = Pink_Ghost.COLOR
+
 
 #* Utilities functions
 def aabb_collision(a: tuple[float, float, float, float], b: tuple[float, float, float, float]) -> bool:
