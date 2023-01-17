61520e29d107100246710abf3fd35deb2cfdc275
Hamada-Gado <ahmedgado0100@gmail.com>
2023-01-17 22:14:27 +0200

    add player controll, but problem in rendring
    images in formate svg not working propebly bounds of image
    is wierd will change to normal images png formate
    
    add add_coordinate function in game_object class

--- game_object/game_object.py
+++ game_object/game_object.py
@@ -36,16 +36,21 @@ class Game_Object(ABC):
         self.rect: pg.Rect = pg.Rect(self.coordinate.x, self.coordinate.y, self.width, self.height)
         self.hit_box: pg.Rect = pg.Rect(self.coordinate.x, self.coordinate.y, self.width - self.offset_x, self.height - self.offset_y)
         
-        self.setcoordinate(x, y)
+        self.set_coordinate(x, y)
         
     @abstractmethod
     def update(self) -> None: ...
     @abstractmethod
     def draw(self) -> None: ...
 
-    def setcoordinate(self, x: int | None = None, y: int | None = None):
+    def set_coordinate(self, x: float | int | None = None, y: float | int | None = None):
         self.coordinate.x = x or self.coordinate.x
         self.coordinate.y = y or self.coordinate.y
         
         self.rect.topleft    = (self.coordinate.x, self.coordinate.y) # type: ignore
         self.hit_box.topleft = (self.coordinate.x + self.offset_x/2, self.coordinate.y + self.offset_y/2) # type: ignore
+    
+    def add_coordinate(self, x: float | int = 0, y: float | int = 0):
+        self.coordinate.x += x
+        self.coordinate.y += y
+        self.set_coordinate()
--- game_object/pacman.py
+++ game_object/pacman.py
@@ -6,28 +6,59 @@ from .game_object import Game_Object
 
 class PacMan(Game_Object):
     
+    UP: str = "up"
+    DOWN: str = "down"
+    LEFT: str = "left"
+    RIGHT: str = "right"
+    
     # images
     images: list[pg.surface.Surface] = list()
     for i in range(3):
         images.append(pg.image.load(f"res/pacman/image{i}.svg"))
               
-    _ = images.copy()
-    _.reverse()
-    images.extend(_)
-    print(images)
+    tmp = images.copy()
+    tmp.reverse()
+    images.extend(tmp)
+    del tmp
         
     speed: int = 100
     
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         f"""{super().__doc__}"""
         super().__init__(*args, **kwargs)
-        
+
         self.current_frame: float = 0
-        self.frame_rate: float = 55.5
+        self.frame_rate: float = 5.5
+        self.direction: str = PacMan.RIGHT
 
     def update(self) -> None:
+        keys = pg.key.get_pressed()
+        
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.direction = PacMan.UP
+            self.add_coordinate(y= -self.speed*self.master.delta_time)
+        elif keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.direction = PacMan.DOWN
+            self.add_coordinate(y= self.speed*self.master.delta_time)
+        elif keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.direction = PacMan.LEFT
+            self.add_coordinate(x= -self.speed*self.master.delta_time)
+        elif keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.direction = PacMan.RIGHT
+            self.add_coordinate(x= self.speed*self.master.delta_time)
+        
         self.current_frame += (self.frame_rate * self.master.delta_time) 
         self.current_frame = self.current_frame if self.current_frame < 6 else 0
     
     def draw(self) -> None:
-        self.master.window.blit(self.images[int(self.current_frame)], self.rect)
+        image = self.images[int(self.current_frame)]
+        if self.direction == PacMan.UP:
+            image = pg.transform.rotate(surface= image, angle= 90)
+        elif self.direction == PacMan.DOWN:
+            image = pg.transform.rotate(surface= image, angle= -90)
+        elif self.direction == PacMan.LEFT:
+            image = pg.transform.rotate(surface= image, angle= 180)
+        elif self.direction == PacMan.RIGHT:
+            image = image
+        
+        self.master.window.blit(image, self.rect)
